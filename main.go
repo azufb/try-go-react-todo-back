46f84060b682b4e229221b49e6a7f8f9587d2950
azufb <curiousazunas@gmail.com>
2024-03-11 23:15:05 +0900

    ちょっと書いてみた

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "net/http"
 
+type homeHandler struct{}
+
+// ServeHTTP implements http.Handler.
+func (h *homeHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
+	panic("unimplemented")
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.Handle("/", &homeHandler{})
+
+	http.ListenAndServe(":8080", mux)
 }
