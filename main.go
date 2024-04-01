77ed871190384459de7086074fd13ec03a7f7d4f
azufb <curiousazunas@gmail.com>
2024-04-01 21:52:31 +0900

    mainとrouterを準備

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"os"
+	"try-go-react-todo-back/router"
+)
+
+func main() {
+
+	s := router.NewServer()
+
+	if err := s.Start(":80"); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ router/router.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func NewServer() *echo.Echo {
+	e := echo.New()
+
+	return e
+}
