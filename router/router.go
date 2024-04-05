a3a298e40b9133299301353a82f54517d2df20f2
azufb <curiousazunas@gmail.com>
2024-04-05 22:49:11 +0900

    さんぷるじっそう

--- router/router.go
+++ router/router.go
@@ -1,11 +1,29 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware" // バージョン不一致でCORSエラー吐くのでv4指定
 )
+
+// Helo Worldを返すAPI
+// https://pkg.go.dev/github.com/labstack/echo
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!")
+}
 
 func NewServer() *echo.Echo {
 	e := echo.New()
+
+	// middleware
+	e.Use(middleware.CORS())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet},
+	}))
+
+	e.GET("/hello", hello)
 
 	return e
 }
