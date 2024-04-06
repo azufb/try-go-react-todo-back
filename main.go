44b26c0c9f2674c286ad0001416ff5cf935b9fbb
azufb <curiousazunas@gmail.com>
2024-04-06 15:48:07 +0900

    関数の枠組み作った

--- main.go
+++ main.go
@@ -8,6 +8,29 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// DBつないでToDoアプリを実装したい
+// DBからのデータ取得・DBにデータ挿入・DBのデータ編集・DBのデータ削除をやる
+
+func todos() error {
+	// 取得
+	return nil
+}
+
+func create() error {
+	// データ挿入
+	return nil
+}
+
+func update() error {
+	// データ編集
+	return nil
+}
+
+func delete() error {
+	// データ削除
+	return nil
+}
+
 func main() {
 	e := echo.New()
 
